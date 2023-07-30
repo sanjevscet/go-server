6d8974e5abff3a8b91e7999e8b549ad3997bf0a2
Sanjeev Kumar <sanjeevscet@gmail.com>
2023-07-30 13:38:10 +0530

    Adding validator

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -5,24 +5,52 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 	"scet.com/utils"
 )
 
 type User struct {
 	ID        string    `json:"id"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
+	FirstName string    `json:"first_name" validate:"required,min=3,max=15"`
+	LastName  string    `json:"last_name" validate:"required,min=3,max=15"`
+	Email     string    `json:"email" validate:"required,email"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
 var users = []User{}
 
+func validateUser(user User) validator.ValidationErrors {
+	validate := validator.New()
+	err := validate.Struct(user)
+	if err != nil {
+		return err.(validator.ValidationErrors)
+	}
+	return nil
+}
+
 func CreateUserHandler(w http.ResponseWriter, r *http.Request) {
 	var newUser User
 	if err := json.NewDecoder(r.Body).Decode(&newUser); err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	// Validate the User struct
+	validationErrors := validateUser(newUser)
+	if len(validationErrors) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+
+		// Format validation errors as JSON with field names as keys
+		errorMap := make(map[string]string)
+		for _, err := range validationErrors {
+			fieldName := err.Field()
+			errorMessage := err.Error()
+			errorMap[fieldName] = errorMessage
+		}
+
+		json.NewEncoder(w).Encode(errorMap)
 		return
 	}
 
